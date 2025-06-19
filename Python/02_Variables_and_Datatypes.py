6acc876dfef94fc73663c42123cab7abcce927db
tusharchoudhary2427 <tushar.choudhary2427@gmail.com>
2025-06-19 23:24:10 +0530

    Add my Python file

--- Python/02_Variables_and_Datatypes.py
+++ Python/02_Variables_and_Datatypes.py
@@ -5,3 +5,11 @@ a = 1 # => a is variable which contain value 1
 b = 2 # => b is variable which contain value 2
 
 print(a + b) # => print the sum of a and b
+
+# data types
+
+a = 1 # 1 is integer
+a = 1.0 # 1.0 is float
+a = "hello" # "hello" is string
+a = True # True is boolean
+
