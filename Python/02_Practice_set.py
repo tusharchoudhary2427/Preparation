831f2ffc6878bc8e39d190f51205fccde62bca91
tusharchoudhary2427 <tushar.choudhary2427@gmail.com>
2025-06-21 22:26:23 +0530

    Added a new py file

---
+++ Python/02_Practice_set.py
@@ -0,0 +1,36 @@
+# Ques. 1 -> Write a python program to add two numbers. 
+
+a = 1
+b = 3
+c = (a + b)
+
+print (c) 
+
+# Ques. 2 -> Write a python program to find remainder when a number is divided by z.
+
+a = 43
+b = 5
+
+print (a % b)
+
+# Ques. 3 -> Check the type of variable assigned using input () function.
+a = "69"    
+print (type(a))
+
+# Ques. 4 -> Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80 
+a = 34
+b = 80
+  
+print(a > b)
+
+# Ques. 5 -> Write a python program to find an average of two numbers entered by the user.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = (a + b) / 2
+print(c)
+
+# Ques. 6 -> Write a python program to calculate the square of a number entered by the user.
+a = int(input("Enter a number: "))
+c = a ** 2
+print(c)
+
