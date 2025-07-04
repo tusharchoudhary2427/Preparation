d8c9664706a8c5efe423b974378264f90d77f1e5
tusharchoudhary2427 <tushar.choudhary2427@gmail.com>
2025-07-04 23:17:15 +0530

    Added some new line of code

--- Python/08_Functions_&_Recursions.py
+++ Python/08_Functions_&_Recursions.py
@@ -10,13 +10,31 @@ def avg(): # This is a function definition.
 
 avg() # This is a function call. 
 
-# Quick Quiz -> Write a program to greet a user with “Good day” using functions
 
-def goodDay():
-    name = input("Enter your name: ")
+
+#FUNCTIONS WITH ARGUMENTS -> Write a program to greet a user with “Good day” using functions
+
+def goodDay(name, ending):
     print("Good day, " + name)
-    
-goodDay()
+    print(ending)
+    return "done"
 
-# Types of Functions -> There are two types of functions in Python: Built-in functions and User-defined functions.
+a = goodDay("Tushar", "Thanks")
+print (a)
 
+# DEFAULT PARAMETER VALUE -> If a function has a parameter with a default value, you can call the function without passing a value for that parameter.
+
+def greet(name = "stranger"):
+    greet()
+    greet("harry") 
+
+
+# RECURSION -> Recursion is a function which calls itself
+
+def factorial(n):
+    if(n == 1 or n == 0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("Enter Your Number: "))
+print(f"The factorial of this number is: {factorial(n)}")
